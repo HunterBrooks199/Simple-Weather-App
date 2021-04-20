f71292ad5a652098002ea1fc900c62c5dcf76040
HunterBrooks199 <hunterbrooks7@yahoo.com>
2021-04-20 17:26:27 -0600

    Initial Commit

---
+++ Simple Weather App/Simple_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Weather_AppApp.swift
+//  Simple Weather App
+//
+//  Created by Hunter Brooks on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
